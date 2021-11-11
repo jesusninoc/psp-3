121334f76a72cf09bba235ed9392c0f05de0f0a5
angelrp2404 <angel.romero@cesjuanpablosegunedo.es>
2021-11-11 19:01:41 +0100

    rentacar
    rentacar

---
+++ Rent a Car/src/Hilos/Hilo.java
@@ -0,0 +1,18 @@
+package Hilos;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Hilo extends Thread {
+
+    public Hilo(){
+    }
+
+    public void run(){
+
+        String timeStamp = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
+
+        System.out.println(timeStamp);
+    }
+
+}
---
+++ Rent a Car/src/Hilos/Time.java
@@ -0,0 +1,13 @@
+package Hilos;
+
+public class Time extends Hilo {
+
+    public static void main (String[] args) {
+
+        Hilo time;
+
+        time = new Hilo();
+
+        time.start();
+    }
+}
